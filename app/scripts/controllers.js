b7e7a87c37d0cf804efce61052d6c04b7bce9e70
Park Sunhee <sunny790821@gmail.com>
2015-12-25 16:24:02 +0900

    ngRoute is not complete

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('confusionApp')
+
+    .controller('ContactController', ['$scope',function($scope) {
+
+    }])
+    .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
+
+        var dish= menuFactory.getDish(parseInt($routeParams.id,10));
+        $scope.dish = dish;
+    }])
     .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
 
